7ca07dac28146bbc007e9eadb121e7286a4bde02
Dzmitriy Krasiuk <strixmymail@gmail.com>
2022-09-07 22:02:31 +0300

    feat: Add Task2

---
+++ src/lesson2/Task2.java
@@ -0,0 +1,10 @@
+package lesson2;
+
+public class Task2 {
+    public static void main(String[] args) {
+        var w = 10;
+        var h = 8;
+        System.out.println((w + h) * 2);
+        System.out.println(w * h);
+    }
+}
