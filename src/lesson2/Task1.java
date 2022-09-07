8db0ef33e839160c6091f0a789f5904402bc2029
Dzmitriy Krasiuk <strixmymail@gmail.com>
2022-09-07 21:59:08 +0300

    feat: Add Task1

---
+++ src/lesson2/Task1.java
@@ -0,0 +1,10 @@
+package lesson2;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int x = 11;
+        int y = 6;
+        System.out.println(x / y);
+        System.out.println(x % y);
+    }
+}
