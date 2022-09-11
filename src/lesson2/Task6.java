d4611740c497c023d22d9a2bf2104e721315a772
Dzmitriy Krasiuk <strixmymail@gmail.com>
2022-09-11 19:47:28 +0300

    feat: Add Task6

---
+++ src/lesson2/Task6.java
@@ -0,0 +1,10 @@
+package lesson2;
+
+public class Task6 {
+    public static void main(String[] args) {
+        double number = 11.7;
+        int checkNumber = (int) number;
+        boolean numberReal = number % checkNumber != 0;
+        System.out.println(numberReal);
+    }
+}
