01e8be5942427763df07ded802a459acfaa540a1
Dzmitriy Krasiuk <strixmymail@gmail.com>
2022-09-11 20:42:39 +0300

    feat: changed Task05

--- src/lesson2/Task5.java
+++ src/lesson2/Task5.java
@@ -2,9 +2,9 @@ package lesson2;
 
 public class Task5 {
     public static void main(String[] args) {
-        var number = 11.05;
-        int x = (int) number;
-        if ((number - x) > 0) {
+        var number = 7.33;
+        int check = (int) number;
+        if ((number - check) > 0) {
             System.out.println("Число имеет вещественную часть");
         } else {
             System.out.println("Число не имеет вещественную часть");
