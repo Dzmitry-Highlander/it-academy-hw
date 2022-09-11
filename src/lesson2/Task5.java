5c2bf14ba33059ea3bf61b20ed28bd25ca62f2be
Dzmitriy Krasiuk <strixmymail@gmail.com>
2022-09-11 20:32:48 +0300

    feat: changed Task05

--- src/lesson2/Task5.java
+++ src/lesson2/Task5.java
@@ -4,7 +4,8 @@ public class Task5 {
     public static void main(String[] args) {
         double number = 11.7;
         int checkNumber = (int) number;
-        boolean numberReal = number % checkNumber != 0;
-        System.out.println(numberReal);
+        double numberReal = number % checkNumber;
+        if(number % checkNumber != 0)
+            System.out.println(numberReal + " есть вещественная часть");
     }
 }
