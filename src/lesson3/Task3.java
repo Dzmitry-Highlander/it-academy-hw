f10462baa0642cba09789fc540c45efb384fc735
Dzmitry_Highlander <strixmymail@gmail.com>
2022-09-13 20:49:41 +0300

    feat: add Task3

---
+++ src/lesson3/Task3.java
@@ -0,0 +1,22 @@
+package lesson3;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int timeOfDay = 19;
+        String helloPhrase;
+
+        if (timeOfDay >= 0 && timeOfDay <= 6) {
+            helloPhrase = "Доброй ночи!";
+        } else if (timeOfDay >= 7 && timeOfDay <= 12) {
+            helloPhrase = "Доброе утро!";
+        } else if (timeOfDay >= 13 && timeOfDay <= 18) {
+            helloPhrase = "Добрый день!";
+        } else if (timeOfDay >= 19 && timeOfDay <= 24) {
+            helloPhrase = "Добрый вечер!";
+        } else {
+            helloPhrase = "Нет такого времени суток";
+        }
+
+        System.out.println(helloPhrase);
+    }
+}
