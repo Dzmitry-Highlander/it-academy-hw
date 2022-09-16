21f56e2de095dba45d726f85bbc82a36b242c56c
Dzmitriy Krasiuk <strixmymail@gmail.com>
2022-09-16 07:53:08 +0300

    feat: add Task1 - Task4

---
+++ src/lesson4/Task1.java
@@ -0,0 +1,13 @@
+package lesson4;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String numbers0To10 = "";
+
+        for (int i = 0; i <= 10; i++) {
+            numbers0To10 += i + " ";
+        }
+
+        System.out.print(numbers0To10);
+    }
+}
---
+++ src/lesson4/Task4.java
@@ -0,0 +1,25 @@
+package lesson4;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int firstNumber = 1;
+        int secondNumber = 1;
+        int multiplicationResult;
+        String multiplicationTable = "";
+
+        while (firstNumber <= 10){
+            while (secondNumber <= 10) {
+                multiplicationTable += firstNumber + " x ";
+                multiplicationTable += secondNumber + " = ";
+                multiplicationResult = firstNumber * secondNumber;
+                multiplicationTable += multiplicationResult + "\n";
+                secondNumber++;
+            }
+            multiplicationTable += "\n";
+            secondNumber = 1;
+            firstNumber++;
+        }
+
+        System.out.println(multiplicationTable);
+    }
+}
