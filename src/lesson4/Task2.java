d4e836c32bfa173c25570af51a0fcc16f6857986
Dzmitry_Highlander <strixmymail@gmail.com>
2022-10-02 06:38:06 +0300

    feat: refactor Task2

--- src/lesson4/Task2.java
+++ src/lesson4/Task2.java
@@ -2,12 +2,12 @@ package lesson4;
 
 public class Task2 {
     public static void main(String[] args) {
-        int numbersToOut = 0;
+        int numbersToOut = 10;
         String numbers0To10 = "";
 
-        while (numbersToOut <= 10) {
+        while (numbersToOut >= 0) {
             numbers0To10 += numbersToOut + " ";
-            numbersToOut++;
+            numbersToOut--;
         }
 
         System.out.println(numbers0To10);
